fb282b541915b391553120a338954da11b80804e
Bianco Jean-François <bianco.jfrancois@gmail.com>
2013-12-19 15:41:25 +0100

    Planete update in minimap

--- Planete_minimap.tcl
+++ Planete_minimap.tcl
@@ -1,19 +1,34 @@
 inherit PlaneteMiniMap_P Presentation
-method PlaneteMiniMap_P constructor {control miniMapCanvas x y radius} {
+method PlaneteMiniMap_P constructor {control kernel miniMapCanvas x y radius} {
 	this inherited $control
 	set this(miniMapCanvas) $miniMapCanvas
+	set this(kernel) $kernel
 	set this(id) [$this(miniMapCanvas) create oval [expr {($x-$radius)/2}] [expr {($y-$radius)/2}] [expr {($x+$radius)/2}] [expr {($y+$radius)/2}] -outline #000 -fill black]
-  
+  	$this(kernel) Subscribe_after_Update_planet $objName "$this(control) updatePositionPlanete \$this(D_planets)"
+}
+
+method PlaneteMiniMap_P updatePositionPlanete {D_planets id} {
+	set x [dict get $D_planets $id x]
+	set y [dict get $D_planets $id y]
+	set radius [dict get $D_planets $id radius]
+	$this(miniMapCanvas) coords $this(id) [expr {($x-$radius)/2}] [expr {($y-$radius)/2}] [expr {($x+$radius)/2}] [expr {($y+$radius)/2}]
 }
 
 inherit PlaneteMiniMap Control
-method PlaneteMiniMap constructor {parent miniMapCanvas x y radius} {
-  PlaneteMiniMap_P ${objName}_P $objName $miniMapCanvas $x $y $radius
+method PlaneteMiniMap constructor {parent kernel miniMapCanvas x y radius} {
+  PlaneteMiniMap_P ${objName}_P $objName $kernel $miniMapCanvas $x $y $radius
   
 
   #Héritage
   this inherited $parent "" ${objName}_P ""
 
 }
-# Generate_PAC_accessors Control Planete_Map_A Planete_Map_P ${objName};
 
+method PlaneteMiniMap updatePositionPlanete {D_planets} {
+	$this(presentation) updatePositionPlanete $D_planets [this get_id]
+}
+
+
+method PlaneteMiniMap get_id {} {
+	return [$this(parent) get_id]
+}
--- planete.tcl
+++ planete.tcl
@@ -1,7 +1,7 @@
 source Planete_map.tcl
 source Planete_minimap.tcl
 source Planete_info.tcl
-source assesor_generator.tcl
+source utils.tcl
 
 inherit Planete_A Abstraction
 method Planete_A constructor {control kernel x y radius density} {
@@ -13,6 +13,13 @@ method Planete_A constructor {control kernel x y radius density} {
   set this(density) $density
   this addPlaneteToKernel $x $y $radius $density
   
+}
+
+
+
+method Planete_A addPlaneteToKernel {x y radius density} {
+ 	set this(id) [$this(kernel) Add_new_planet $x $y $radius $density]
+	$this(kernel) Subscribe_after_Destroy_planet $objName "after 10 {$this(control) dispose}"
 }
 
 method Planete_A set_x {x} {
@@ -36,6 +43,7 @@ method Planete_A dispose {} {
 	this inherited
 }
 
+Generate_PAC_accessors Planete Planete_A "" id
 
 inherit Planete Control
 method Planete constructor {parent kernel mapCanvas minimapCanvas infoCanvas x y radius density} {
@@ -46,23 +54,13 @@ method Planete constructor {parent kernel mapCanvas minimapCanvas infoCanvas x y
 	# Declaration PAC fils
 	# pour un Planete, on a 1 Planete map, 1 Planete mini map, 1 Planete info
 	PlaneteMap ${objName}_PM $objName $mapCanvas $x $y $radius
-	PlaneteMiniMap ${objName}_PMM $objName $minimapCanvas $x $y $radius
+	PlaneteMiniMap ${objName}_PMM $objName $kernel $minimapCanvas $x $y $radius
 	PlaneteInfo ${objName}_PI $objName $infoCanvas $x $y
 	
 	this inherited $parent ${objName}_A "" [list ${objName}_PM ${objName}_PMM ${objName}_PI]
 }
 
 
-
-method Planete_A addPlaneteToKernel {x y radius density} {
- 	set this(id) [$this(kernel) Add_new_planet $x $y $radius $density]
-	$this(kernel) Subscribe_after_Destroy_planet $objName "after 10 {$this(control) dispose}"
-	# $this(kernel) Subscribe_after_Update_planet  $objName "
-	# 	 if {\$id == \"$this(id)\"} {
-	# 		 {$objName} set_position \[list \[dict get \$this(D_planets) \$id x\] \[dict get \$this(D_planets) \$id y\]\]
-	# 		}
-		# "
-}
 
 method Planete set_position {position} {
     lassign $position x y
