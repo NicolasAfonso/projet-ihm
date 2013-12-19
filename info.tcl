b0a1512eac3cd068719999636f185d844a0bb82c
Bianco Jean-François <bianco.jfrancois@gmail.com>
2013-12-19 22:18:34 +0100

    Ajout joueur et vaisseau

--- info.tcl
+++ info.tcl
@@ -1,6 +1,9 @@
+source utils.tcl
 inherit Info_P Presentation
 method Info_P constructor {control infoCanvas} {
   this inherited $control
+
+  ########### Object information ##############
   set this(infoCanvas) $infoCanvas
   set this(typeLabel) [label $this(infoCanvas).typeLabel -justify left -text ""]
 
@@ -12,49 +15,87 @@ method Info_P constructor {control infoCanvas} {
   pack $this(positionLabel) -anchor n -fill both
   pack $this(infoLabel) -anchor n -fill both
 
-   set this(addShipLabel) [label $this(infoCanvas).addShipLabel -justify left -text "Ajouter vaisseau"]
-   set this(shipNameLabel) [label $this(infoCanvas).shipNameLabel -justify left -text "Nom vaisseau : "]
-   set this(shipName) [entry $this(infoCanvas).shipName -justify left]
+  ########### Add Ship ########################
+  set this(addShipLabel) [label $this(infoCanvas).addShipLabel -justify left -text "Ajouter vaisseau"]
+  set this(shipNameLabel) [label $this(infoCanvas).shipNameLabel -justify left -text "Nom vaisseau : "]
+  set this(shipName) [entry $this(infoCanvas).shipName -justify left]
+  set this(playerSNameLabel) [label $this(infoCanvas).playerSNameLabel -justify left -text "Nom du joueur : "]
+  set this(shipPlayer) [entry $this(infoCanvas).shipPlayer -justify left]
 
-   set this(playerSNameLabel) [label $this(infoCanvas).playerSNameLabel -justify left -text "Nom du joueur : "]
-   set this(shipPlayer) [entry $this(infoCanvas).shipPlayer -justify left]
+  set this(coordXLabel) [label $this(infoCanvas).coordXLabel -justify left -text "X : "]
+  set this(coordSX) [entry $this(infoCanvas).coordSX -justify left]
 
-   set this(coordXLabel) [label $this(infoCanvas).coordXLabel -justify left -text "X : "]
-   set this(coordSX) [entry $this(infoCanvas).coordSX -justify left]
+  set this(coordYLabel) [label $this(infoCanvas).coordYLabel -justify left -text "Y : "]
+  set this(coordSY) [entry $this(infoCanvas).coordSY -justify left]
 
-   set this(coordYLabel) [label $this(infoCanvas).coordYLabel -justify left -text "Y : "]
-   set this(coordSY) [entry $this(infoCanvas).coordSY -justify left]
+  pack $this(addShipLabel) -anchor n -fill both
+  pack $this(shipNameLabel) -anchor n -fill both
+  pack $this(shipName) -anchor n -fill both
 
-   pack $this(addShipLabel) -anchor n -fill both
-   pack $this(shipNameLabel) -anchor n -fill both
-   pack $this(shipName) -anchor n -fill both
+  pack $this(playerSNameLabel) -anchor n -fill both
+  pack $this(shipPlayer) -anchor n -fill both
 
-   pack $this(playerSNameLabel) -anchor n -fill both
-   pack $this(shipPlayer) -anchor n -fill both
+  pack $this(coordXLabel) -anchor n -fill both
+  pack $this(coordSX) -anchor n -fill both
 
-   pack $this(coordXLabel) -anchor n -fill both
-   pack $this(coordSX) -anchor n -fill both
+  pack $this(coordYLabel) -anchor n -fill both
+  pack $this(coordSY) -anchor n -fill both
+  set this(addShip) [ttk::button $this(infoCanvas).addShip -text "Ajouter mon vaisseau!" -command "$this(control) addShip"]
+  pack $this(addShip) -anchor n -fill both
 
-   pack $this(coordYLabel) -anchor n -fill both
-   pack $this(coordSY) -anchor n -fill both
 
-   set this(addShip) [ttk::button $this(infoCanvas).addShip -text "Ajouter mon vaisseau!" -command "$this(control) addShip $this(shipName) $this(shipPlayer) $this(coordSX) $this(coordSY)"]
-   pack $this(addShip) -anchor n -fill both
- #   set this(diametre) [entry $this(infoCanvas).diametre -justify left]
-	# set this(densite) [entry $this(infoCanvas).densite -justify left]
+  ##### Add player ##############################
+  set this(addPlayerLabel) [label $this(infoCanvas).addPlayerLabel -justify left -text "Ajouter joueur"]
+  set this(playerNameLabel) [label $this(infoCanvas).playerNameLabel -justify left -text "Nom Joueur : "]
+  set this(namePlayer) [entry $this(infoCanvas).namePlayer -justify left]
 
-   set this(addCommandLabel) [label $this(infoCanvas).addCommandLabel -justify left -text "Starfleet command"]
-   set this(fire) [ttk::button $this(infoCanvas).fire -text "Feu !" -command "$this(control) fire"]
-   pack $this(addCommandLabel) -anchor n -fill both
-   pack $this(fire) -anchor n -fill both
+  set this(addColorPlayerLabel) [label $this(infoCanvas).addColorPlayerLabel -justify left -text "Couleur"]
+  set this(addColorPlayer) [entry $this(infoCanvas).addColorPlayer -justify left]
+  set this(addPlayer) [ttk::button $this(infoCanvas).addPlayer -text "Ajouter mon joueur!" -command "$this(control) addPlayer"]
+
+  pack $this(addPlayerLabel) -anchor n -fill both
+  pack $this(playerNameLabel) -anchor n -fill both
+  pack $this(namePlayer) -anchor n -fill both
+  pack $this(addColorPlayerLabel) -anchor n -fill both
+  pack $this(addColorPlayer) -anchor n -fill both
+  pack $this(addPlayer) -anchor n -fill both
+
+
+  #####Command Fire #############################
+  set this(addCommandLabel) [label $this(infoCanvas).addCommandLabel -justify left -text "Starfleet command"]
+  set this(fire) [ttk::button $this(infoCanvas).fire -text "Feu !" -command "$this(control) fire"]
+  pack $this(addCommandLabel) -anchor n -fill both
+  pack $this(fire) -anchor n -fill both
 	# set this(diametreLabel) [label $this(infoCanvas).diametreLabel -justify left -text "Diametre:"]
 	# set this(densiteLabel) [label $this(infoCanvas).densiteLabel -justify left -text "Densite:"]
 	# pack $this(diametreLabel) -anchor n -fill both
 	# pack $this(diametre) -anchor n -fill both
 }
 
-method Info addShip {player name x y} {
-	$this(parent) addShip $player $name $x $y
+
+method Info_P addShip {} {
+	$this(control) addShipOnKernel [$this(shipPlayer) get] [$this(shipName) get] [$this(coordSX) get]  [$this(coordSY) get]
+}
+
+method Info addShip {} {
+	$this(presentation) addShip
+}
+
+method Info addShipOnKernel {player ship x y} {
+	$this(parent) addShip $player $ship $x $y
+}
+
+method Info_P addPlayer {} {
+	puts "[$this(addColorPlayer) get]"
+	$this(control) addPlayerOnKernel [$this(namePlayer) get] [$this(addColorPlayer) get]
+}
+
+method Info addPlayer {} {
+	$this(presentation) addPlayer
+}
+
+method Info addPlayerOnKernel {namePlayer color} {
+	$this(parent) addPlayer $namePlayer $color
 }
 
 method Info fire {} {
--- univers.tcl
+++ univers.tcl
@@ -9,26 +9,6 @@ inherit Univers_A Abstraction
 method Univers_A constructor {control kernel mapCanvas minimapCanvas infoCanvas} {
 	this inherited $control
 	set this(mapCanvas) $mapCanvas
-	# $kernel Subscribe_after_Destroy_planet A {puts "Destroy planet $id"; $this(mapCanvas) delete $id}
-	# $kernel Subscribe_after_Update_planet  A {
-	# set x [dict get $this(D_planets) $id x]
-	# set y [dict get $this(D_planets) $id y]
-	# set radius [dict get $this(D_planets) $id radius]
-	# # this(mapCanvas) coords $id [expr $x - $radius] [expr $y - $radius] [expr $x + $radius] [expr $y + $radius]
-	# }	
-
-	# # $kernel Subscribe_after_Destroy_ship A {puts "Destroy ship $id"; $this(mapCanvas) delete $id}
-	# $kernel Subscribe_after_Destroy_ship A "puts {Destroy ship \$id}; $this(mapCanvas) delete \$id"
-
-# 	$kernel Subscribe_after_Add_new_ship ALEX {$this(mapCanvas) delete $rep; $this(mapCanvas) create oval [expr $x - $radius] [expr $y - $radius] [expr $x + $radius] [expr $y + $radius] -fill red -tags [list ALEX $rep]}
-# $kernel Subscribe_after_Destroy_ship ALEX {puts "Destroy ship $id"; $this(mapCanvas) delete $id}
-# $kernel Subscribe_after_Update_ship  ALEX {
-# 	set x [dict get $this(D_players) $id_player D_ships $id x]
-# 	set y [dict get $this(D_players) $id_player D_ships $id y]
-# 	set radius [dict get $this(D_players) $id_player D_ships $id radius]
-# 	$this(mapCanvas) coords $id [expr $x - $radius] [expr $y - $radius] [expr $x + $radius] [expr $y + $radius]
-# }
-
 	
 }
 
@@ -46,26 +26,22 @@ method Univers constructor {parent kernel mapCanvas minimapCanvas infoCanvas} {
 	MiniMap ${objName}_MMAP $objName $minimapCanvas $kernel
 	Map ${objName}_MAP $objName $mapCanvas $kernel
 
-	#Vaisseaux parent kernel mapCanvas minimapCanvas infoframe
-	#Vaisseau ${objName}_V1 $objName $kernel $mapCanvas $minimapCanvas $infoCanvas Player_1 90 90 red
-
-	
-
 	#Heritage
 	this inherited $parent ${objName}_A "" [list ${objName}_I ${objName}_MMAP ${objName}_MAP ] 
 }
 
 method Univers addPlanete {name x y radius density} {
-	#Planetes parent kernel mapCanvas minimapCanvas infoCanvas x y radius density
-	#Planete ${objName}_P1 $objName $kernel $mapCanvas $minimapCanvas $infoCanvas 50 50 40 40
 	Planete ${objName}_$name $objName $this(kernel) $name $this(mapCanvas) $this(minimapCanvas) $this(infoCanvas) $x $y $radius $density
 }
 
 method Univers addShip {player name x y} {
-Vaisseau ${objName}_$name $objName $this(kernel) $this(mapCanvas) $this(minimapCanvas) $this(infoCanvas) $this(parent)_J_$player $name $x $y 
+	Vaisseau ${objName}_$name $objName $this(kernel) $this(mapCanvas) $this(minimapCanvas) $this(infoCanvas) $this(parent)_J_$player $name $x $y 
 }
 
 method Univers fire {} {
 	$this(kernel) Start_fire
 }
 
+method Univers addPlayer {namePlayer color} {
+	$this(parent) addPlayer $namePlayer $color
+}
